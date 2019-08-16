15ee6b10c97b36213cdb9b43ca93d8e6dd2f005e
muckSponge <5405629+muckSponge@users.noreply.github.com>
2019-08-16 19:14:05 +1000

    Don’t use System.Expressions w/ IL2CPP
    See #4

--- UnityAsync/Assets/UnityAsync/Extensions.cs
+++ UnityAsync/Assets/UnityAsync/Extensions.cs
@@ -11,15 +11,21 @@ namespace UnityAsync
 {
 	public static class Extensions
 	{
+		#if !ENABLE_IL2CPP
+		
 		static readonly Func<Object, IntPtr> ObjectPtrGetter;
 		
-		static Extensions()
-		{
-			ReflectionUtility.GenerateFieldGetter("m_CachedPtr", out ObjectPtrGetter);
-		}
+		static Extensions() => ReflectionUtility.GenerateFieldGetter("m_CachedPtr", out ObjectPtrGetter);
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		internal static bool IsAlive(this Object o) => ObjectPtrGetter(o) != IntPtr.Zero;
+		
+		#else
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static bool IsAlive(this Object o) => o != null;
+		
+		#endif
 		
 		/// <summary>
 		/// Link the <see cref="UnityAsync.IAwaitInstruction"/>'s lifespan to a <see cref="UnityEngine.Object"/> and
